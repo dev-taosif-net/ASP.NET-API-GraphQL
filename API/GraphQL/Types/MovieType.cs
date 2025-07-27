3f9be9f2073d4993cf16e41b11d45ed1eb9eafeb
Taosif Bin Murtaza <dev.taosif.net@gmail.com>
2025-07-28 05:57:12 +0600

    Add GraphQL support and define Movie GraphQL type
    Added NuGet packages for GraphQL, OpenAPI, and EF Core SQL Server.
    Introduced a global using directive for `GraphQL.Types`.
    Created `MovieType` class to define a GraphQL object type for
    the `Movie` entity, including fields like `Id`, `Name`, and
    `Genre`. Prepared for future `reviews` field functionality.

---
+++ API/GraphQL/Types/MovieType.cs
@@ -0,0 +1,20 @@
+﻿namespace API.GraphQL.Types;
+
+public class MovieType : ObjectGraphType<Entities.Movie>
+{
+    public MovieType()
+    {
+        Name = "Movie";
+        Description = "A movie entity";
+        Field(x => x.Id).Description("The unique identifier of the movie.");
+        Field(x => x.Name).Description("The name of the movie.");
+        Field(x => x.Description).Description("A brief description of the movie.");
+        Field(x => x.LaunchDate).Description("The release date of the movie.");
+        Field(x => x.Genre).Description("The genre of the movie.");
+        //Field<ListGraphType<MovieReviewType>>(
+        //    "reviews",
+        //    resolve: context => context.Source.Reviews
+        //);
+    }
+}
+
